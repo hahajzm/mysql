a936fb4e5a358928325da83c5fdc9cfba880c68e
xiaomeng <xiaomeng199596@163.com>
2017-06-20 21:21:08 -0400

    zeifan

---
+++ addschool.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <mysql/mysql.h>
+#include "cgic.h"
+
+int cgiMain()
+{
+
+	fprintf(cgiOut, "Content-type:text/html;charset=utf-8\n\n");
+
+	char cno[32] = "\0";
+	char sub[16] = "\0";
+	char credit[32] = "\0";
+
+	int status = 0;
+
+	status = cgiFormString("cno",  cno, 32);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get ncno error!\n");
+		return 1;
+	}
+
+	status = cgiFormString("sub",  sub, 16);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get sub error!\n");
+		return 1;
+	}
+
+	status = cgiFormString("credit",  credit, 32);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get credit error!\n");
+		return 1;
+	}
+
+	//fprintf(cgiOut, "name = %s, age = %s, stuId = %s\n", name, age, stuId);
+
+	int ret;
+	char sql[128] = "\0";
+	MYSQL *db;
+
+	//初始化
+	db = mysql_init(NULL);
+	if (db == NULL)
+	{
+		fprintf(cgiOut,"mysql_init fail:%s\n", mysql_error(db));
+		return -1;
+	}
+
+	//连接数据库
+	db = mysql_real_connect(db, "127.0.0.1", "root", "123456", "stu",  3306, NULL, 0);
+	if (db == NULL)
+	{
+		fprintf(cgiOut,"mysql_real_connect fail:%s\n", mysql_error(db));
+		mysql_close(db);
+		return -1;
+	}
+
+
+
+  /*strcpy(sql, "create table student(id int not null primary key, name varchar(20) not null, age int not null,tel int not null,sex varchar(20) not null)");
+	if ((ret = mysql_real_query(db, sql, strlen(sql) + 1)) != 0)
+	{
+		if (ret != 1)
+		{
+			fprintf(cgiOut,"mysql_real_query fail:%s\n", mysql_error(db));
+			mysql_close(db);
+			return -1;
+		}
+	}*/
+
+
+
+	sprintf(sql, "insert into school values(%d, '%s', %d)", atoi(cno), sub, atoi(credit));
+	if (mysql_real_query(db, sql, strlen(sql) + 1) != 0)
+	{
+		fprintf(cgiOut, "%s\n", mysql_error(db));
+		mysql_close(db);
+		return -1;
+	}
+
+	fprintf(cgiOut, "add course ok!\n");
+	mysql_close(db);
+	return 0;
+}
